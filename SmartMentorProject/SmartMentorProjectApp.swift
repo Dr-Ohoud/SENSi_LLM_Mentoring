415794362c0cde906d947b1161cceb6517fcc918
Shahad Bagarish <shahad.bagarish@gmail.com>
2025-02-03 17:14:20 +0300

    Initial Commit

---
+++ SmartMentorProject/SmartMentorProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartMentorProjectApp.swift
+//  SmartMentorProject
+//
+//  Created by Shahad Bagarish on 03/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SmartMentorProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
